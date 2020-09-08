a8806ee98cb9c9b2b1e2a3463bda13877ce5ec3e
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-09-08 14:53:34 +0000

    add deploy.sh and systemd file

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+
+echo 'Restarting Go...'
+cd $DIR/webapp/go/
+make clean
+make
+sudo systemctl stop torb.golang.service
+cd $DIR
+sudo systemctl restart torb.golang.service
+sudo systemctl enable torb.golang.service
+echo 'Restarted!'
+
+sudo cp $DIR/systemd/* /etc/systemd/system/
+sudo systemctl daemon-reload
+
+echo 'Updating config file...'
+sudo cp "$DIR/nginx.conf" /etc/nginx/nginx.conf
+# sudo cp "$DIR/redis.conf" /etc/redis/redis.conf
+sudo cp "$DIR/mysqld.cnf" /etc/mysql/mysql.conf.d/mysqld.cnf
+echo 'Updated config file!'
+
+echo 'Restarting middleware services...'
+# sudo systemctl restart redis.service
+# Save cache
+sudo systemctl restart mysql.service
+sudo systemctl restart nginx.service
+echo 'Restarted!'
+
+echo 'Rotating files'
+sudo bash -c 'cp /var/log/nginx/access.log /var/log/nginx/access.log.$(date +%s).$(git rev-parse HEAD) && echo > /var/log/nginx/access.log'
+sudo bash -c 'cp /tmp/mysql-slow.sql /tmp/mysql-slow.sql.$(date +%s).$(git rev-parse HEAD) && echo > /tmp/mysql-slow.sql'
+echo 'Rotated!'
